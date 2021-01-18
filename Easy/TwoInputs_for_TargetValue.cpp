7a343d0b3824c08a6c61497fefec99465cb687a5
Jay Malanker <jjmalankar@gmail.com>
2021-01-18 18:59:18 +0530

    Create TwoInputs_for_TargetValue.cpp

---
+++ Easy/TwoInputs_for_TargetValue.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+class Ok
+{
+    public:
+    int i=0, posi1=0, posi2=0;
+    float tar=0.0, x=0.0, y=0.0, z=0.0;
+    float a[999];
+    void inptNo()
+    {
+        cout<<"No. of Inputs?\n";
+        cin>>i;
+    }
+    void inpt()
+    {
+        cout<<"\nEnter your inputs\n";
+        for(int j=0;j<i;j++)
+        {
+            cin>>a[j];
+        }
+    }
+    void tr()
+    {
+        cout<<"\nEnter your target\n";
+        cin>>tar;
+    }
+    void calc()
+    {
+        for(int k=0;k<i;k++)
+        {
+            for(int l=0;l<i;l++)
+            {
+                if(k==l)
+                {
+                    break;
+                }
+                x=a[k];
+                y=a[l];
+                z=x+y;
+                if(z==tar)
+                {
+                    posi1=k;
+                    posi2=l;
+                    break;
+                }
+            }
+            if(z==tar)
+            {
+                break;
+            }
+        }
+    }
+    void display()
+    {
+        cout<<"\n["<<posi2<<","<<posi1<<"]\n";
+    }
+};
+int main()
+{
+    Ok Obj;
+    Obj.inptNo();
+    Obj.inpt();
+    Obj.tr();
+    Obj.calc();
+    Obj.display();
+    return 0;
+}
