3ddc2fe1f1c01f02a40ae26102b5789bbe27a8aa
Jay Malanker <jjmalankar@gmail.com>
2021-02-01 21:44:54 +0530

    Create Sqrt().cpp

---
+++ Easy/Sqrt().cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int x= 0;
+    double sqr= 0.0;
+    cout<<"Enter Value\n";  cin>>x;
+    sqr= pow(x, 0.5);
+    int res= sqr;
+    cout<<"\n"<<res;
+}
