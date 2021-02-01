fb71bdc7a6119c2e356e92dd242dc4a3d8e9709b
Jay Malanker <jjmalankar@gmail.com>
2021-02-01 22:32:00 +0530

    Create Valid_Parantheses.cpp

---
+++ Easy/Valid_Parantheses.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    string str= "";
+    int len= 0;
+    bool Valid= false;
+    int i= 0, j= 0;
+    cout<<"Enter String\t"; cin>>str;
+    len= str.length();
+    for(i= 0; i< len; i++)
+    {
+        if(str[i]=='(' || str[i]=='{' || str[i]=='[')
+            j++;
+        else if(str[i]==')' || str[i]=='}' || str[i]==']')
+            j--;
+        else
+            continue;
+    }
+    if(j==0)
+        Valid= true;
+    if(Valid)
+        cout<<"\nValid Parentheses in the Given String";
+    else
+        cout<<"\nInvalid Parentheses in the Given String";
+    return 0;
+}
