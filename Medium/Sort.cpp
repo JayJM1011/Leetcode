cd065a2c90ec48e1b8cc43c160695e7c33843e5a
Jay Malanker <jjmalankar@gmail.com>
2021-02-01 22:04:04 +0530

    Create Sort.cpp

---
+++ Medium/Sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int No= 0;                                          //<Inputs>
+    int i= 0, j= 0;
+    cout<<"Enter No. of Elements\t";    cin>>No;
+    int arr[No];
+    cout<<"\nEnter Elements\n";
+    for(i= 0; i< No; i++)
+        cin>>arr[i];                                    //</inputs>
+    for(i= No- 1; i>=0; i--)
+    {
+        for(j= 0; j< i; j++)
+        {
+            if(arr[j]> arr[j+ 1])
+                swap(arr[j], arr[j+ 1]);
+        }
+    }
+    for(i= 0; i< No; i++)                               //<Outputs>
+    {
+        if(i==No-1)
+            cout<<arr[i];
+        else
+            cout<<arr[i]<<", ";
+    }                                                   //</Outputs>
+    return 0;
+}
