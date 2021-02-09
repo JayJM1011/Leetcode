f184ea4b7ed1d444b48215c5a68dfb12a5fa540a
Jay Malanker <jjmalankar@gmail.com>
2021-02-10 01:17:21 +0530

    Update Remove_Element.cpp

--- Easy/Remove_Element.cpp
+++ Easy/Remove_Element.cpp
@@ -3,21 +3,21 @@ using namespace std;
 
 int main()
 {
-    int  i=0, j=0, k=0, l=0;
-    int n=0, rem=0;
+    int  i= 0, j= 0, k= 0, l= 0;
+    int n= 0, rem= 0;
     cout<<"Enter the No. of Elements\t\t";      cin>>n;
-    int ar[n];
+    int ar[n]= {0};
     cout<<"\nEnter Elements\n";
-    for(i=0;i<n;i++)
+    for(i= 0; i< n; i++)
         cin>>ar[i];
     cout<<"\nEnter No. you want Removed\n";     cin>>rem;
-    for(i=0;i<n;i++)
+    for(i= 0; i< n; i++)
     {
         if(ar[j]==rem)
         {
-            for(k=j;k<n-i;k++)
+            for(k= j; k< n- i; k++)
             {
-                ar[k]=ar[k+1];
+                ar[k]= ar[k+1];
             }
             j--;
             l++;
@@ -25,7 +25,7 @@ int main()
         j++;
     }
     cout<<"\n";
-    for(i=0;i<n-l;i++)
+    for(i= 0; i< n- l; i++)
     {
         cout<<ar[i]<<" ";
     }
