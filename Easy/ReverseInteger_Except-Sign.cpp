db15e5e5110d2058d20c5e77d6c51ee3141c23f5
Jay Malanker <jjmalankar@gmail.com>
2021-02-10 01:21:28 +0530

    Update ReverseInteger_Except-Sign.cpp

--- Easy/ReverseInteger_Except-Sign.cpp
+++ Easy/ReverseInteger_Except-Sign.cpp
@@ -6,18 +6,18 @@ using namespace std;
 class RevInt
 {
     public:
-    int x=0, y=0, z=0, in=0, i=0, j=0, k=0, l=0, temp=0, sum=0;
-    int a=0, b=0;                       //Counter for no. of \0 in string
-    string str="";
-    bool c=false;
-    char m='\0';
+    int x= 0, y= 0, z= 0, in= 0, i= 0, j= 0, k= 0, l= 0, temp= 0, sum= 0;
+    int a= 0, b= 0;                       //Counter for no. of \0 in string
+    string str= "";
+    bool c= false;
+    char m= '\0';
     void In()
     {
         cin>>in;
     }
     void ConAndNoOfChar()
     {
-        str=str+to_string(in);
+        str= str+ to_string(in);
         while(str[i])
             i++;
         i--;
@@ -25,38 +25,36 @@ class RevInt
     }
     void Rev()
     {
-        z=i/2;
+        z= i/ 2;
         if(str[0]=='-')
         {
-            x=1;
-            y=1;
-            k=1;
+            x= 1;
+            y= 1;
+            k= 1;
         }
-        for (x;x<=z;x++)
+        for (x; x<=z; x++)
         {
-            temp=str[x];
-            str[x]=str[j];
-            str[j]=temp;
+            temp= str[x];
+            str[x]= str[j];
+            str[j]= temp;
             j--;
         }
-        j=0;
+        j= 0;
     }
     void RemZero()
     {
-        for(k;k<=i;k++)
+        for(k; k<=i; k++)
         {
             if(str[k]=='0')
             {
-                str[k]='\0';
+                str[k]= '\0';
                 a++;
             }
             else
-            {
                 break;
-            }
         }
-        b=str.length();
-        b=b-a-y;
+        b= str.length();
+        b= b- a- y;
         cout<<"\nDigits after 0s Removed \t"<<b;
         b--;
     }
@@ -68,20 +66,18 @@ class RevInt
     {
         if(y==1)
             c=true;
-        for(y;y<=i;y++)
+        for(y; y<=i; y++)
         {
-            m=str[y];
-            l=m-48;
+            m= str[y];
+            l= m- 48;
             if(l!=-48)
             {
-                sum=sum+(l*pow(10,b));
+                sum= sum+ (l* pow(10,b));
                 b--;
             }
         }
         if(c)
-        {
             sum= -sum;
-        }
     }
     void display2()
     {
