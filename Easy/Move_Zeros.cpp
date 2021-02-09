729534292fc4334a53c16023ba20fb92c86dfef9
Jay Malanker <jjmalankar@gmail.com>
2021-02-10 01:08:17 +0530

    Update Move_Zeros.cpp

--- Easy/Move_Zeros.cpp
+++ Easy/Move_Zeros.cpp
@@ -1,59 +1,34 @@
 #include <iostream>
 using namespace std;
 
-class MoveZ
-{
-    public:
-    int no= 0, n= 0, arr[100], temp= 0;
-    int i= 0, j= 0;
-    void in()
-    {
-        cout<<"Enter No. of Elements\t";
-        cin>>no;
-        n=no;
-    }
-    void arrIn()
-    {
-        while(i<no)
-        {
-            cin>>arr[i];
-            i++;
-        }
-    }
-    void shifter()
-    {
-        for (i= 0; i< no;i++)
-        {
-            temp= arr[i];
-            if(temp==0)
-            {
-                for(j=i;j< no-1;j++)
-                {
-                    temp= arr[j];
-                    arr[j]= arr[j+1];
-                    arr[j+1]= temp;
-                }
-                no--;
-            }
-        }
-        i=0;
-    }
-    void disp()
-    {
-        while(i<n)
-        {
-            cout<<arr[i];
-            i++;
-        }
-    }
-};
-
 int main()
 {
-    MoveZ Obj;
-    Obj.in();
-    Obj.arrIn();
-    Obj.shifter();
-    Obj.disp();
+    int no= 0, n= 0, arr[100], temp= 0;
+    int i= 0, j= 0;
+    cout<<"Enter No. of Elements\t";    cin>>no;    n=no;       //Inputs
+    while(i<no)
+    {
+        cin>>arr[i];
+        i++;
+    }
+    for (i= 0; i< no;i++)                                       //Logic
+    {
+        temp= arr[i];
+        if(temp==0)                                             //Condition to Bubble if 0 is found
+        {
+            for(j=i;j< no-1;j++)
+            {
+                temp= arr[j];
+                arr[j]= arr[j+1];
+                arr[j+1]= temp;
+            }
+        }
+    }
+    i=0;
+    while(i<n)                                                  //Display
+    {
+        cout<<arr[i];
+        i++;
+    }
     return 0;
 }
