bec4ae2ac747b5d7fec4ec166af19a357af219b0
Jay Malanker <jjmalankar@gmail.com>
2021-02-11 22:35:04 +0530

    Update Palindrome_BooleanReturn.cpp

--- Easy/Palindrome_BooleanReturn.cpp
+++ Easy/Palindrome_BooleanReturn.cpp
@@ -2,54 +2,27 @@
 #include <string.h>
 using namespace std;
 
-class Pal
-{
-    public:
-    int y= 0, z= 0, i= 0;
-    string str= "";
-    bool Ans= false;
-
-    void in()
-    {
-        cout<<"\nEnter Number to be checked\n";
-        cin>>str;
-    }
-    void digits()
-    {
-        while(str[i])
-            i++;
-        y= i;
-        z= i/ 2;
-    }
-    void Pali()
-    {
-        for(int j= 1; j<=z; j++)
-        {
-            if(str[j]!=str[y])
-            {
-                cout<<"\nNot a Palindrome No., never can be";
-                break;
-            }
-            else
-            {
-                y--;
-            }
-        }
-    }
-    void ifPali()
-    {
-        if(y<=(z- 1))
-        {
-            cout<<"\nIt is a Palindrome No. my dude";
-        }
-    }
-};
 int main()
 {
-    Pal Obj;
-    Obj.in();
-    Obj.digits();
-    Obj.Pali();
-    Obj.ifPali();
+    int y= 0, i= 0;
+    string str= "";
+    bool Ans= false;
+    cout<<"\nEnter Number to be checked\n";
+        cin>>str;
+    while(str[i])
+        i++;
+    y= i;
+    for(int j= 1; j<=i/ 2; j++)
+    {
+        if(str[j]!=str[y])
+        {
+            cout<<"\nNot a Palindrome No., never can be";
+            break;            
+        }
+        else
+            y--;
+    }
+    if(y<=(i/ 2- 1))
+        cout<<"\nIt is a Palindrome No. my dude";
     return 0;
 }
