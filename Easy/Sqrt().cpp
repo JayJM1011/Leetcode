5351d828441fba9bedfe7331941ebfe5d4112f55
Jay Malanker <jjmalankar@gmail.com>
2021-02-12 00:21:47 +0530

    Update Sqrt().cpp

--- Easy/Sqrt().cpp
+++ Easy/Sqrt().cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include <cmath>
-using namespace std;
 
 int main()
 {
     int x= 0;
-    double sqr= 0.0;
-    cout<<"Enter Value\n";  cin>>x;
-    sqr= pow(x, 0.5);
-    int res= sqr;
-    cout<<"\n"<<res;
+    std::cout<<"Enter Value\n";     std::cin>>x;
+    std::cout<<"\n"<<pow(x, 0.5);
 }
