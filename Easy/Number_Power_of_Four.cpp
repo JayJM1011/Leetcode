1fd2ba11a2e48078c5207bdd1d7f3dd9e8303718
Jay Malanker <jjmalankar@gmail.com>
2021-02-13 00:01:25 +0530

    Create Number_Power_of_Four.cpp

---
+++ Easy/Number_Power_of_Four.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+bool PowOf4(int Num)
+{
+    if(Num<=0)
+        return false;
+    if(Num==1)
+        return true;
+    unsigned int x= 4;
+    while(x<=Num)
+    {
+        if(Num% x==0 && Num/ (x/ 4)==4)
+            return true;
+        x= x* 4;
+    }
+    return false;
+}
+int main()
+{
+    int Num= 0;
+    std::cout<<"Enter number\t";  std::cin>>Num;
+    bool y= PowOf4(Num);
+    if(y)
+    {
+        std::cout<<"Power of 4";
+        return 0;
+    }
+    std::cout<<"Not a Power of 4";
+    return 0;
+}
+//1162261466
