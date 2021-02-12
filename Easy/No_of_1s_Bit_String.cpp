4c9d809f2477fff3757d5abb63df0678a8edf521
Jay Malanker <jjmalankar@gmail.com>
2021-02-13 00:21:26 +0530

    Create No_of_1s_Bit_String.cpp

---
+++ Easy/No_of_1s_Bit_String.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    string str= "";
+    int i= 0, j= 0;
+    cout<<"Enter Binary String\t";    cin>>str;
+    int len= str.length();
+    for(i= 0; i< len; i++)
+    {
+        if(str[i]=='1')
+            j++;
+    }
+    cout<<"Given String has "<<j<<" no of 1s";
+    return 0;
+}
