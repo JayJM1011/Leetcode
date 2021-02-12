22379ef4ab4718290402ae97f055fe1b8d27291d
Jay Malanker <jjmalankar@gmail.com>
2021-02-12 11:03:38 +0530

    Update Longest_Substring_without_Repeating_Characters.cpp

--- Medium/Longest_Substring_without_Repeating_Characters.cpp
+++ Medium/Longest_Substring_without_Repeating_Characters.cpp
@@ -6,27 +6,22 @@ using namespace std;
 int main()
 {
     string str= "";
-    int arr[256];                               //For all Characters Possible
+    int arr[256]= {0};                          //For all Characters Possible
     int l= 0, r= 0;                             //Left and Right Pointers
     int k= 0, Max= 0;
     cout<<"Enter String\n"; cin>>str;
     int len= str.length();
-    while(l< 256)
-    {
-        arr[l]= 0;
-        l++;
-    }
     l= 0;
     while(r< len)
     {
         arr[str[r]]++;                          //As char can be taken as ASCII int values, they can be used for indexes
         while (arr[str[r]]> 1)                  //> 1 shows that char has been Repeated
         {
-            arr[str[l]]--;                      //re-establishing initial state
+            arr[str[l]]--;                      //Re-establishing initial state
             l++;
         }
         k= r- l+ 1;
-        Max= max(Max, k);
+        Max= max(Max, k);                       //Checking wheter this substring was the Longest
         k= 0;
         r++;
     }
