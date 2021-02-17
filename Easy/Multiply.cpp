1e8de0a07238f7068d3b080b77c39c4cc40d7b84
Jay Malanker <jjmalankar@gmail.com>
2021-02-17 13:43:50 +0530

    Update Multiply.cpp

--- Easy/Multiply.cpp
+++ Easy/Multiply.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int No= 0, i= 0, temp= 1, Pro= 1;
     cout<<"Enter No. of Elements\t";    cin>>No;
-    for(i= 0; i< No; i++)
+    for( ; i< No; i++)
     {
         cin>>temp;
         Pro= Pro* temp;
