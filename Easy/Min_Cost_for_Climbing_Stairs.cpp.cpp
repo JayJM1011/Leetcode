987cba1daf80c08b9167c5ea6669cba99c181c49
Jay Malanker <jjmalankar@gmail.com>
2021-02-17 23:21:38 +0530

    Update Min_Cost_for_Climbing_Stairs.cpp.cpp

--- Easy/Min_Cost_for_Climbing_Stairs.cpp.cpp
+++ Easy/Min_Cost_for_Climbing_Stairs.cpp.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 #define cout std::cout
 #define cin std::cin
+
+int Min(int a, int b)
+{
+    if(a< b)
+        return a;
+    return b;       //Cause even if b==a we need to skip a
+}
