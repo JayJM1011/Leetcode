18b24b3b8c846f5f280b3f3e0f7a786332cac882
Jay Malanker <jjmalankar@gmail.com>
2021-02-18 00:48:35 +0530

    Update No_of_Ways_to_Climb_Stairs.cpp

--- Easy/No_of_Ways_to_Climb_Stairs.cpp
+++ Easy/No_of_Ways_to_Climb_Stairs.cpp
@@ -2,6 +2,14 @@
 #define cout std::cout
 #define cin std::cin
 
+int Climb(int a, int n)
+{
+    if(a> n)
+        return 0;
+    if(a==n)
+        return 1;
+    return Climb(a+ 1, n)+ Climb(a+ 2, n);  //Recursion ftw
+}
 int main()
 {
     system("cls");
