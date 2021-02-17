6fd2353c9f94e88e20d47e9b03ec3d2d6d0b6d9a
Jay Malanker <jjmalankar@gmail.com>
2021-02-17 23:22:34 +0530

    Update Min_Cost_for_Climbing_Stairs.cpp.cpp

--- Easy/Min_Cost_for_Climbing_Stairs.cpp.cpp
+++ Easy/Min_Cost_for_Climbing_Stairs.cpp.cpp
@@ -8,3 +8,13 @@ int Min(int a, int b)
         return a;
     return b;       //Cause even if b==a we need to skip a
 }
+int main()
+{
+    system("cls");
+    int i= 0;
+    int NoOfStairs= 0, Sum= 0, Mini= 0;
+    cout<<"\nEnter the Number of Stairs\t";     cin>>NoOfStairs;
+    int Stairs[NoOfStairs]= {0};
+    system("cls");
+    return 0;
+}
